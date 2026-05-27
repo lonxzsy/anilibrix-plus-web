8ed65414a594c7cd80899fb68b48e27ca822b153
plat1numdev <nameres7878@gmail.com>
2026-05-27 20:24:29 +0200

    chore: remove temporary fix scripts

--- fix_sub_overlay.js
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-let c = fs.readFileSync('src/components/SubtitleOverlay.vue','utf8');
-c = c.replace(/style\.position = p as any\r\n\s+emitStyle\(\)/, 'style.position = p as any; emitStyle()');
-c = c.replace(/style\.align = a as any\r\n\s+emitStyle\(\)/, 'style.align = a as any; emitStyle()');
-fs.writeFileSync('src/components/SubtitleOverlay.vue', c, 'utf8');
-console.log('done');
